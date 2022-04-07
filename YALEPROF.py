466d587779a4d177168dee05099368e0425e3b58
Sarlay <60151189+Sarlay@users.noreply.github.com>
2022-04-07 09:30:39 +0000

    Update YALEPROF.py

--- YALEPROF.py
+++ YALEPROF.py
@@ -17,10 +17,9 @@ def write_temp_files():
         Sample = msgBox("YA LA PROF qui t'espionne" & vbCrLf & vbCrLf & "VOULEZ VOUS FUIR ????",16+32+4, "YA LE PROF !!!")
         Select Case Sample
             Case 6
-                
                 Dim objShell
                 Set objShell = CreateObject("WScript.Shell")
-                objShell.Run("""C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"" ""https://fakeupdate.net/win10ue/""")
+                objShell.Run("""C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"" ""https://taozhiyu.gitee.io/fake/win10ue/win10ue.html""")
                 Wscript.Sleep 100
                 objShell.SendKeys "{F11}"
                 WScript.Quit
@@ -28,18 +27,30 @@ def write_temp_files():
                 wScript.Echo "Ok mais le prof te surveille toujours"
         End Select''')
         f.close()
+    with open('C:\\Users\\ELVE~1\\AppData\\Local\\Temp\\pasla.vbs', 'w+') as f:
+        f.write('''Sample = msgBox("LA PROF EST PARTI" & vbCrLf & vbCrLf & "Vous n'etes plus surveillé",0+64, "LE PROF EST PARTI !!!")''')
+        f.close()
 
 
 write_temp_files()
 
 
+monitored = False
 while True:
     p = subprocess.Popen('powershell.exe -ExecutionPolicy RemoteSigned -file "C:\\Users\\ELVE~1\\AppData\\Local\\Temp\\YALEPROF.ps1"', stdout=PIPE)
     text = p.communicate()[0]
-    if b"Established" in text:
+    if b"Established" in text and monitored is False: # teacher started to monitor you
         subprocess.call(['cscript.exe', f'{directory}\\popup.vbs'])
         sleep(10)
-    else:
+        monitored = True # update state to monitored
+    elif b"Established" in text and monitored is True:
+        print("toujours surveillé, prochaine update dans 10s")
         sleep(10)
+    elif b"Established" not in text and monitored is True:
+        subprocess.call(['cscript.exe', f'{directory}\\pasla.vbs']) # popup teacher left
+        monitored = False # update, teacher left
+    elif b"Established" not in text and monitored is False:
+        print("Ya pas le prof trkl, update dans 5 secondes")
+        sleep(5)
 
 #'powershell.exe -ExecutionPolicy RemoteSigned -file "C:\Users\Elève\Documents\site\YALEPROF.ps1"', stdout=sys.stdout)
